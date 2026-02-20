cec74775928c8abc10467b2a921b9a698abfe5c0
Julian Garritano <julian.garritano@gmail.com>
2026-02-20 09:43:19 -0500

    Fix issues with slot c08+

--- src/main/config/index.ts
+++ src/main/config/index.ts
@@ -15,6 +15,7 @@ export const CONFLICT_WHITELIST_PATTERNS = [
   'READ ME.txt',
   'Preview.webp',
   '.DS_Store',
+  'Readme.txt',
 ];
 
 export const TEMP_FOLDERS = ['fightplanner-downloads', 'fightplanner-extract'];
